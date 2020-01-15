f07b6b2e795c77cc46d6c2ca032383417caaf51c
Joshua Tokle <joshtok@amazon.com>
2020-01-15 09:03:17 -0600

    Add JMH microbenchmarks to package
    This commit adds the bencmarking code and Maven configuration to build
    and run benchmarks. The README is updated with instructions.

---
+++ src/test/benchmarks/com/amazon/randomcutforest/RandomCutForestBenchmark.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.randomcutforest;
+
+import com.amazon.randomcutforest.returntypes.DensityOutput;
+import com.amazon.randomcutforest.returntypes.DiVector;
+import com.amazon.randomcutforest.util.NormalMixtureTestData;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@Fork(value = 1)
+@State(Scope.Benchmark)
+public class RandomCutForestBenchmark {
+
+    public final static int DATA_SIZE = 50_000;
+
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        @Param({"1", "8", "64"})
+        int dimensions;
+
+        @Param({"50", "100"})
+        int numberOfTrees;
+
+        @Param({"256"})
+        int sampleSize;
+
+        @Param({"false", "true"})
+        boolean parallelExecutionEnabled;
+
+        double[][] data;
+        RandomCutForest forest;
+
+        @Setup(Level.Trial)
+        public void setUpData() {
+            NormalMixtureTestData testData = new NormalMixtureTestData();
+            data = testData.generateTestData(DATA_SIZE, dimensions);
+        }
+
+        @Setup(Level.Iteration)
+        public void setUpForest() {
+            forest = RandomCutForest.builder()
+                    .numberOfTrees(numberOfTrees)
+                    .sampleSize(sampleSize)
+                    .dimensions(dimensions)
+                    .parallelExecutionEnabled(parallelExecutionEnabled)
+                    .randomSeed(99)
+                    .build();
+        }
+    }
+
+    private RandomCutForest forest;
+
+    @Benchmark
+    @OperationsPerInvocation(DATA_SIZE)
+    public RandomCutForest updateOnly(BenchmarkState state) {
+        double[][] data = state.data;
+        forest = state.forest;
+
+        for (int i = 0; i < data.length; i++) {
+            forest.update(data[i]);
+        }
+
+        return forest;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(DATA_SIZE)
+    public RandomCutForest updateAndGetAnomalyScore(BenchmarkState state, Blackhole blackhole) {
+        double[][] data = state.data;
+        forest = state.forest;
+        double score = 0.0;
+
+        for (int i = 0; i < data.length; i++) {
+            score = forest.getAnomalyScore(data[i]);
+            forest.update(data[i]);
+        }
+
+        blackhole.consume(score);
+        return forest;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(DATA_SIZE)
+    public RandomCutForest updateAndGetAnomalyAttribution(BenchmarkState state, Blackhole blackhole) {
+        double[][] data = state.data;
+        forest = state.forest;
+        DiVector vector = new DiVector(forest.getDimensions());
+
+        for (int i = 0; i < data.length; i++) {
+            vector = forest.getAnomalyAttribution(data[i]);
+            forest.update(data[i]);
+        }
+
+        blackhole.consume(vector);
+        return forest;
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(DATA_SIZE)
+    public RandomCutForest updateAndGetBasicDensity(BenchmarkState state, Blackhole blackhole) {
+        double[][] data = state.data;
+        forest = state.forest;
+        DensityOutput output = new DensityOutput(forest.getDimensions(), forest.getSampleSize());
+
+        for (int i = 0; i < data.length; i++) {
+            output = forest.getSimpleDensity(data[i]);
+            forest.update(data[i]);
+        }
+
+        blackhole.consume(output);
+        return forest;
+    }
+}
---
+++ src/test/benchmarks/com/amazon/randomcutforest/TreeUpdaterBenchmark.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.randomcutforest;
+
+import java.util.Arrays;
+
+import com.amazon.randomcutforest.sampler.SimpleStreamSampler;
+import com.amazon.randomcutforest.tree.RandomCutTree;
+import com.amazon.randomcutforest.util.NormalMixtureTestData;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@Fork(value = 1)
+@State(Scope.Benchmark)
+public class TreeUpdaterBenchmark {
+
+    public static final int DATA_SIZE = 50_000;
+
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        @Param({"1", "8", "64"})
+        int dimensions;
+
+        @Param({"256"})
+        int sampleSize;
+
+        @Param({"0.0", "1e-5"})
+        double lambda;
+
+        // @Param({"false", "true"})
+        @Param({"true"})
+        boolean storeSequenceIndexesEnabled;
+
+        @Param({"false", "true"})
+        boolean centerOfMassEnabled;
+
+        double[][] data;
+        TreeUpdater treeUpdater;
+
+        @Setup(Level.Trial)
+        public void setUpData() {
+            NormalMixtureTestData testData = new NormalMixtureTestData();
+            data = testData.generateTestData(DATA_SIZE, dimensions);
+        }
+
+        @Setup(Level.Iteration)
+        public void setUpTree() {
+            SimpleStreamSampler sampler = new SimpleStreamSampler(sampleSize, lambda, 99);
+            RandomCutTree tree = RandomCutTree.builder()
+                    .randomSeed(101)
+                    .storeSequenceIndexesEnabled(storeSequenceIndexesEnabled)
+                    .centerOfMassEnabled(centerOfMassEnabled)
+                    .build();
+            treeUpdater = new TreeUpdater(sampler, tree);
+        }
+    }
+
+    private TreeUpdater tree;
+
+    @Benchmark
+    @OperationsPerInvocation(DATA_SIZE)
+    public TreeUpdater update(BenchmarkState state) {
+        double[][] data = state.data;
+        tree = state.treeUpdater;
+        long entriesSeen = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            try {
+                tree.update(data[i], ++entriesSeen);
+            } catch (Exception e) {
+                System.out.println("Point = " + Arrays.toString(data[i]));
+                System.out.println("Sequence Index = " + entriesSeen);
+                throw e;
+            }
+        }
+
+        return tree;
+    }
+}
