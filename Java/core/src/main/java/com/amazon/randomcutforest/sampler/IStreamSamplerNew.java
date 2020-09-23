17f6e3e33bcfbcf0e7c26308b8837872040c5e06
Sudipto Guha <sudipto@amazon.com>
2020-09-22 22:58:36 -0700

    toddler steps

---
+++ Java/core/src/main/java/com/amazon/randomcutforest/sampler/IStreamSamplerNew.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazon.randomcutforest.sampler;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.amazon.randomcutforest.Sequential;
+
+/**
+ * A sampler that samples from an ordered sequence of points.
+ * 
+ * @param <P> The type representing the points
+ */
+public interface IStreamSamplerNew<P> {
+    /**
+     * Submit a point to the sampler. The sampler implementation determines whether
+     * the point is added to the sample or not.
+     *
+     * This function is not used explicitly in RandomCutForest but is helpful in
+     * testing the samplers.
+     * 
+     * @param point The point submitted to the sampler.
+     * @return true if the point is accepted and added to the sample, false if the
+     *         point is rejected.
+     */
+    default boolean sample(P point, long seqNum) {
+        Optional<Double> result = acceptSample(seqNum);
+        if (result.isPresent()) {
+            addSample(point, result.get(), seqNum);
+            return true;
+        }
+        return false;
+    }
+
+    void addSample(P point, double weight, long seqNum);
+
+    /**
+     * The function decides if the new object elem would be added to the queue.
+     *
+     * @param seqNum sequence number
+     *
+     * @return returns Optional.empty() if the entry is noe accepted; otherwise
+     *         returns the weight
+     */
+
+    Optional<Double> acceptSample(long seqNum);
+
+    /**
+     * @return the list of weighted points currently making up the sample.
+     */
+    List<Sequential<P>> getWeightedSamples();
+
+    /**
+     * @return the point that was evicted from the sample in the most recent call to
+     *         {@link #sample}, or {@code Optional.empty()} if no point was evicted.
+     */
+
+    Optional<Sequential<P>> getEvictedPoint();
+}
