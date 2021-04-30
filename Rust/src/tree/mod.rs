77280019940225d9f5fc32377e699ccd11288ac5
Chris Swierczewski <csw@amazon.com>
2021-04-30 10:54:14 -0700

    Move PointAdder contents to Tree

--- Rust/src/tree/mod.rs
+++ Rust/src/tree/mod.rs
@@ -9,11 +9,8 @@ pub use cut::Cut;
 mod node;
 pub use node::{Internal, Leaf, Node};
 
-mod point_adder;
-pub use point_adder::{AddResult, PointAdder};
-
 mod point_deleter;
 pub use point_deleter::{DeleteResult, PointDeleter};
 
 mod tree;
-pub use tree::{NodeTraverser, Tree};
+pub use tree::{AddResult, NodeTraverser, Tree};
