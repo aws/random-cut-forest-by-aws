61940472bc165e50898cdb5474a145e36cd9a247
Joshua Tokle <joshtok@amazon.com>
2021-06-16 12:50:41 -0700

    Merge main into 2.0 (#250)
    * set version to 1.0 (#58)
    
    * Improve CLI usage instructions (#59)
    
    * Improve CLI usage instructions
    
    Expand upon the CLI instructions with an example and information
    on where to find additional CLI applications. Adds an example data
    file for ease of instruction.
    
    Closes #43
    
    * [rust] Add fundamental types for random cut trees (#169)
    
    * [rust] Add fundamental types for random cut trees
    
    * [rust] Remove unnecessary Python components in Rust implementation .gitignore
    
    * [rust] Cleanup doctests
    
    * [rust] whitespace cleanup
    
    * [rust] Rename package to random_cut_forest
    
    * [rust] Use iterator style for BoundingBox::contains_point
    
    * [rust] quick change to kebab-case
    
    * Rust CI Workflow (#172)
    
    Create a workflow specification for Rust pull requests. Builds package and runs tests.
    
    * [rust] Random Cut Trees (#173)
    
    * [rust] random cut trees
    * Fix documentation and trim whitespace
    * Move PointAdder contents to Tree
    * Move PointDeleter contents to Tree
    * Move point addition and deletion to separate impl blocks
    * Remove use of trait aliases
    
    * [rust] Sampled trees and reservoir samplers (#178)
    
    * [rust] Sampled trees
    
    Implement sampled trees, which are a combination of a point store,
    a reservoir sampler, and a random cut tree.
    
    * Fix re-export and docs
    
    * Rename and change time decays and weights to f32s
    
    * [rust] Implement the random cut forest type (#188)
    
    * [rust] Implement the random cut forest type
    
    This commit only implements `update()` on random cut forests. We also
    implement a random cut forest builder type.
    
    * Remove num_trees as a field and derive from trees vec
    
    * [rust] Basic visitor-based anomaly score (#192)
    
    * [rust] Basic visitor-based anomaly score
    
    * Trim trailing whitespace
    
    * Update sampler test example (#241)
    
    * [Rust] Create a Visitor trait to abstract out scoring algorithms (#217)
    
    * [Rust] Create a Visitor trait to abstract out scoring algorithms
    
    Most scoring algorithms make use of a visitor design pattern. The Visitor trait
    makes it easy to perform node traversals with new algorithms. We also add a
    Tree::traverse() method that makes use of any type that implements Visitor.
    This addition makes the implementation of anomaly_score() much cleaner.
    
    * [Rust] Check for Visitor impl at compile-time
    
    * Create a separate Tree::traverse function
    
    The way node iteration is implemented requires collecting and reversing
    the nodes before applying a visitor. We create an independent traverse
    function specifically for visitors. The existing iter() function is
    left as-is for testing purposes.
    
    Closes #219
    
    * [Rust] Rename algorithm submodule to visitor
    
    * [Rust] Fix docstrings
    
    * [Rust] Add example CLI script on CSV input (#218)
    
    * [Rust] Add CLI-based anomaly score example
    
    * [Rust] Include an example usage CLI script for streaming AD
    
    * [Rust] Add output_after threshold to RCF (#246)
    
    * [Rust] Add output after threshold to RCF
    
    * Add test case for output after
    
    Co-authored-by: John Mazanec <jmazane@amazon.com>
    
    Co-authored-by: Lai <57818076+wnbts@users.noreply.github.com>
    Co-authored-by: Chris Swierczewski <cswiercz@gmail.com>
    Co-authored-by: Chris Swierczewski <csw@amazon.com>
    Co-authored-by: Jack Mazanec <jmazane1@nd.edu>
    Co-authored-by: John Mazanec <jmazane@amazon.com>

---
+++ Rust/src/tree/bounding_box.rs
@@ -0,0 +1,291 @@
+extern crate num_traits;
+use num_traits::{Float, Zero};
+
+use std::fmt;
+use std::iter::Sum;
+
+/// Bounding box on collections on points.
+///
+/// Given a set of *d*- dimensional points, a bounding box is the smallest *d*-
+/// dimensional rectangular prism containing all of these points. A bounding box
+/// is represented by two vectors
+///
+/// # Examples
+///
+/// ```
+/// use random_cut_forest::BoundingBox;
+///
+/// // create a new bounding box from a single point
+/// let point: Vec<f32> = vec![1.0, 2.0];
+/// let bbox = BoundingBox::new_from_point(&point);
+/// assert_eq!(bbox.min_values(), &point);
+/// assert_eq!(bbox.max_values(), &point);
+///
+/// // create a second bounding box by merging the first one with another point
+/// let new_point = vec![3.0, -2.0];
+/// let merged_bbox = BoundingBox::merged_box_with_point(&bbox, &new_point);
+/// println!("{}", &merged_bbox);   // BoundingBox ((1.0, -2.0), (3.0, 2.0))
+///
+/// // confirm that the two points and the first bounding box are contained
+/// // in this larger merged box
+/// assert!(merged_bbox.contains_point(&point));
+/// assert!(merged_bbox.contains_point(&new_point));
+/// assert!(merged_bbox.contains_box(&bbox));
+/// ```
+pub struct BoundingBox<T> {
+    min_values: Vec<T>,
+    max_values: Vec<T>,
+    dimensions: usize,
+    range_sum: T,
+}
+
+impl<T> BoundingBox<T> 
+    where T: Float + Sum
+{
+
+    /// Create a new bounding box from a min values vector and a max values
+    /// vector.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![-1.0, 0.0];
+    /// let max = vec![1.0, 3.0];
+    /// let bbox = BoundingBox::new(&min, &max);
+    /// assert_eq!(bbox.dimensions(), 2);
+    /// assert_eq!(bbox.range_sum(), 5.0);
+    /// ```
+    pub fn new(min_values: &Vec<T>, max_values: &Vec<T>) -> Self {
+        assert_eq!(min_values.len(), max_values.len());
+
+        BoundingBox {
+            min_values: min_values.clone(),
+            max_values: max_values.clone(),
+            dimensions: min_values.len(),
+            range_sum: BoundingBox::compute_range_sum(min_values, max_values),
+        }
+    }
+
+    /// Create a new bounding box from a single point.
+    ///
+    /// The resulting bounding box has no interior: its min values are equal to
+    /// its max values. Therefore, its range sum is zero.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let point: Vec<f32> = vec![1.0, 2.0];
+    /// let bbox = BoundingBox::new_from_point(&point);
+    /// assert_eq!(bbox.min_values(), &point);
+    /// assert_eq!(bbox.max_values(), &point);
+    /// assert_eq!(bbox.dimensions(), 2);
+    /// assert_eq!(bbox.range_sum(), 0.0);
+    /// ```
+    pub fn new_from_point(point: &Vec<T>) -> Self {
+        BoundingBox {
+            min_values: point.clone(),
+            max_values: point.clone(),
+            dimensions: point.len(),
+            range_sum: Zero::zero(),
+        }
+    }
+
+    /// Returns a new bounding box given by the merging of a bounding box with
+    /// a point.
+    ///
+    /// If the point lies inside the bounding box then this returns a clone of
+    /// the same bounding box.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![0.0, 0.0];
+    /// let max = vec![1.0, 1.0];
+    /// let bbox = BoundingBox::new(&min, &max);
+    ///
+    /// let point = vec![0.5, 3.0];
+    /// let merged = BoundingBox::merged_box_with_point(&bbox, &point);
+    /// assert_eq!(merged.max_values(), &vec![1.0, 3.0]);
+    /// assert_eq!(merged.range_sum(), 4.0);
+    ///
+    /// ```
+    pub fn merged_box_with_point(
+        bounding_box: &BoundingBox<T>,
+        point: &Vec<T>) -> Self
+    {
+        let min_values: Vec<T> = bounding_box.min_values().iter()
+            .zip(point)
+            .map(|(&x, &y)| Float::min(x, y))
+            .collect();
+
+        let max_values: Vec<T> = bounding_box.max_values().iter()
+            .zip(point)
+            .map(|(&x, &y)| Float::max(x, y))
+            .collect();
+
+        let dimensions = min_values.len();
+        let range_sum = BoundingBox::compute_range_sum(
+            &min_values, &max_values);
+
+        BoundingBox {
+            min_values: min_values,
+            max_values: max_values,
+            dimensions: dimensions,
+            range_sum: range_sum,
+        }
+    }
+
+    /// Returns a new bounding box given by the merging of two bounding boxes.
+    ///
+    /// The merging of two bounding boxes is given by two points. The first is
+    /// the minimum value in each dimension. The second is the maximum value
+    /// in each dimension. The points contained in both bounding boxes are also
+    /// contained in this large bounding box.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![0.0, 0.0];
+    /// let max = vec![2.0, 2.0];
+    /// let bbox1 = BoundingBox::new(&min, &max);
+    ///
+    /// let min = vec![1.0, 1.0];
+    /// let max = vec![3.0, 4.0];
+    /// let bbox2 = BoundingBox::new(&min, &max);
+    ///
+    /// let merged = BoundingBox::merged_box_with_box(&bbox1, &bbox2);
+    /// assert_eq!(merged.min_values(), &vec![0.0, 0.0]);
+    /// assert_eq!(merged.max_values(), &vec![3.0, 4.0]);
+    /// assert_eq!(merged.range_sum(), 7.0);
+    /// ```
+    pub fn merged_box_with_box(
+        bounding_box1: &BoundingBox<T>,
+        bounding_box2: &BoundingBox<T>) -> Self
+    {
+        let min_values: Vec<T> = bounding_box1.min_values().iter()
+            .zip(bounding_box2.min_values())
+            .map(|(&x, &y)| Float::min(x, y))
+            .collect();
+
+        let max_values: Vec<T> = bounding_box1.max_values().iter()
+            .zip(bounding_box2.max_values())
+            .map(|(&x, &y)| Float::max(x, y))
+            .collect();
+
+        let dimensions = min_values.len();
+        let range_sum = BoundingBox::compute_range_sum(
+            &min_values, &max_values);
+
+        BoundingBox {
+            min_values: min_values,
+            max_values: max_values,
+            dimensions: dimensions,
+            range_sum: range_sum,
+        }
+    }
+
+    /// Get the dimensionality of the bounding box.
+    pub fn dimensions(&self) -> usize { self.dimensions }
+
+    /// Get the vector of min values of the bounding box.
+    pub fn min_values(&self) -> &Vec<T> { &self.min_values }
+
+    /// Get the vector of max values of the bounding box.
+    pub fn max_values(&self) -> &Vec<T> { &self.max_values }
+
+    /// Get the sum across all dimensions of lengths of the bounding box.
+    pub fn range_sum(&self) -> T { self.range_sum }
+
+    /// Returns true if the given point is contained inside the bounding box
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![0.0, 0.0];
+    /// let max = vec![2.0, 2.0];
+    /// let bbox = BoundingBox::new(&min, &max);
+    /// assert!(bbox.contains_point(&vec![1.0, 1.0]));
+    /// assert!(bbox.contains_point(&vec![0.0, 2.0]));
+    /// assert!(!bbox.contains_point(&vec![-1.0, 0.5]));
+    /// assert!(!bbox.contains_point(&vec![1.0, 3.0]));
+    /// ```
+    pub fn contains_point(&self, point: &Vec<T>) -> bool {
+        (0..self.dimensions).all(
+            |i| self.min_values[i] <= point[i] && point[i] <= self.max_values[i])
+    }
+
+    /// Returns true if the given bounding box is contained inside this
+    /// bounding box.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![0.0, 0.0];
+    /// let max = vec![8.0, 8.0];
+    /// let bbox = BoundingBox::new(&min, &max);
+    ///
+    /// let min = vec![0.0, 1.0];
+    /// let max = vec![2.0, 3.0];
+    /// let small_bbox = BoundingBox::new(&min, &max);
+    /// assert!(bbox.contains_box(&small_bbox));
+    ///
+    /// let min = vec![4.0, 6.0];
+    /// let max = vec![9.0, 7.0];
+    /// let med_bbox = BoundingBox::new(&min, &max);
+    /// assert!(!bbox.contains_box(&med_bbox));
+    /// ```
+    pub fn contains_box(&self, bounding_box: &BoundingBox<T>) -> bool {
+        for i in 0..self.dimensions {
+            let min = bounding_box.min_values[i];
+            let max = bounding_box.max_values[i];
+            if min < self.min_values[i] || self.max_values[i] < max {
+                return false;
+            }
+        }
+        true
+    }
+
+    /// Compute the range sum from a pair of min/max value vectors.
+    ///
+    /// The range sum is the sum of the differences between the min values and
+    /// max values of the bounding box across each component. For example, if
+    /// the min values are `[a, b]` and the max values are `[c, d]` then the
+    /// range sum is equal to `(c -a) + (d - b)`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use random_cut_forest::BoundingBox;
+    ///
+    /// let min = vec![1.0, 2.0];
+    /// let max = vec![4.0, 3.0];
+    /// let range_sum = BoundingBox::compute_range_sum(&min, &max);
+    /// assert_eq!(range_sum, 4.0);
+    /// ```
+    pub fn compute_range_sum(min_values: &Vec<T>, max_values: &Vec<T>) -> T {
+        let dimensions = min_values.len();
+        assert_eq!(dimensions, max_values.len());
+
+        (0..dimensions).map(|i| max_values[i] - min_values[i]).sum()
+    }
+}
+
+impl<T> fmt::Display for BoundingBox<T>
+    where T: fmt::Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "BoundingBox ({:?}, {:?})", self.min_values, self.max_values)
+    }
+}
