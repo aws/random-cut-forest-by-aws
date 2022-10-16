e3ade68c204acbee90ed58c0c6185aa8b9d5ca43
Andrew Kane <andrew@ankane.org>
2022-10-16 13:50:13 -0700

    Added support for boxing errors

--- Rust/src/errors.rs
+++ Rust/src/errors.rs
@@ -1,7 +1,20 @@
+use std::error;
+use std::fmt;
+
 /// Errors that can be returned by RCF operations.
 #[derive(Debug, PartialEq)]
 pub enum RCFError {
     InvalidArgument {
         msg: &'static str,
     },
+}
+
+impl error::Error for RCFError {}
+
+impl fmt::Display for RCFError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RCFError::InvalidArgument { msg } => write!(f, "{}", msg),
+        }
+    }
 }
